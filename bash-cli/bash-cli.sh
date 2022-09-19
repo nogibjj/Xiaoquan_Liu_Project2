35b94b92d0735a453d743d37bcdb8e644993d70a
XquanL <112578003+XquanL@users.noreply.github.com>
2022-09-19 21:39:51 +0000

    bash-cli

--- bash-cli/bash-cli.sh
+++ bash-cli/bash-cli.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 echo "Welcome to my Bash CLI"
+echo "================="
+echo "Datasets"
+echo "=================="
+echo "Enter h to see the head of the datasets: "
+echo "Enter t to see the tail of the datasets: "
+echo "Enter q to exit"
+echo -e "\n"
+echo -e "Your Choices \c"
+read Choices
+case "$Choices" in 
+    h) head ;;
+    t) tail ;;
+    q) exit ;;
+esac
 
-# catch first arguments with $1
 
