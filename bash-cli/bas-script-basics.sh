65763b82ff4341817fc94cd952d4524b208614b5
XquanL <112578003+XquanL@users.noreply.github.com>
2022-09-23 15:20:09 +0000

    bash

---
+++ bash-cli/bas-script-basics.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+#This is whhere comments go
+#It can be useful to explain the purpose of your code
+## Note you can also start your script with #!/usr/bin/bash -xv for verbose debugging
+
+#Set strict mode.    Causes shell to exit when a comman fails
+#set -env
+
+#Enables printing of shell inpot lines as they are read
+#set -v
+
+#Enables print of command trace before executing command
+#set -xv
+
+#Set a variable
+variable="one"
+
+echo "This is a script with a variable: $variable"
